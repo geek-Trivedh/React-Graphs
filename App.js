6287f42c1b7b548bed13831881c660ed45f31ecf
Trivedh Jajala <trived@Trived-Kumar-Jajala-B547s-Macbook-Pro.local>
2022-01-12 15:08:01 +0530

    FEAT:added victory line chart

--- App.js
+++ App.js
@@ -1,10 +1,35 @@
 import React from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import {VictoryLine, VictoryChart, VictoryTheme} from 'victory-native';
 
-const App = () => (
-  <SafeAreaView
-    style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-    <Text>Charts</Text>
-  </SafeAreaView>
-);
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <VictoryChart theme={VictoryTheme.material}>
+        <VictoryLine
+          style={{
+            data: {stroke: '#c43a31'},
+            parent: {border: '1px solid #ccc'},
+          }}
+          data={[
+            {x: 1, y: 2},
+            {x: 2, y: 3},
+            {x: 3, y: 5},
+            {x: 4, y: 4},
+            {x: 5, y: 7},
+          ]}
+        />
+      </VictoryChart>
+    </View>
+  );
+};
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5fcff',
+  },
+});
